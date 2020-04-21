ca11c348b3c76f4afe9d269e6f676bcf5a89a4f3
odalebrown <odale_brown@yahoo.com>
2020-04-21 19:04:39 -0400

    Final

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,105 +1,24 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
     template: `
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-              <a class="navbar-brand" href="#">VueJS App</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-        </header>
-    `,
-    data: function() {
-      return {};
-    }
-});
-/* New list Component */
-Vue.component('news-list', {
-template: `
-    <div>
-       <div class = "news">
-            <h2 News </h2>
-            <ul class = "news__list">
-              <li v-for="article in articles"class="news__item">
-                {{ article.title }}
-                {{article.description}}
-                <img :src = "article.urlToImage">
-              </li>
-            </ul>
-        </div>
-      <div class="form-inline d-flex justify-content-center">
-            <div class="form-group mx-sm-3 mb-2">
-                <label class="sr-only" for="search">Search</label>
-                <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-                <p>You are searching for {{ searchTerm }}</p>
-                <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
-             </div>
-      </div>
-    </div>
-    `,
-    created: function(){
-        let self = this;
-        fetch('http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=92b0ce3b21e94a5e9c5b005ba5c6a517')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-            self.articles = data.articles;
-        });
-    },
-    data: function()
-    {
-        return {
-            articles:[],
-            searchTerm: ''
-        }
-    },
-    methods: {
-        searchNews: function() {
-            let self = this;
-            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<api-key>')
-            .then(function(response) {
-                return response.json();
-             })
-             .then(function(data) {
-                 console.log(data);
-                     self.articles = data.articles;
-            });
-        }
-    }
-});
-Vue.component('app-footer', {
-    template: `
-        <footer>
-            <div class="container">
-                <p>Copyright &copy {{ year }} Flask Inc.</p>
-            </div>
-        </footer>
-    `,
-    data: function() {
-        return {
-            year: (new Date).getFullYear()
-        }
-    }
-})
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <a class="navbar-brand" href="#">Lab 7</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
-    }
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/upload">Upload</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    `
 });
 
 Vue.component('app-footer', {
@@ -124,26 +43,97 @@ const Home = Vue.component('home', {
     }
 });
 
-const NotFound = Vue.component('not-found', {
+const Upload = Vue.component("upload-form", {
     template: `
     <div>
-        <h1>404 - Not Found</h1>
+        <div v-if='messageFlag' >
+
+            <div v-if="!errorFlag ">
+                <div class="alert alert-success" >
+                    {{ message }}
+                </div>
+            </div>
+            <div v-else >
+                <ul class="alert alert-danger">
+                    <li v-for="error in message">
+                        {{ error }}
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <h1>Upload Photo</h1>
+        <form id="uploadForm" @submit.prevent="UploadForm" enctype="multipart/form-data">
+            <label>Description:</label><br/>
+            <textarea name='description'></textarea><br/>
+            <label for='photo' class='btn btn-danger'>Browse</label> <span>{{ filename }}</span>
+            <input id="photo" type="file" name='photo' style="display: none" v-on:change = "onFileSelected" /><br/>
+            <input type="submit" value="Upload" class="btn btn-info"/>
+        </form>
     </div>
     `,
-    data: function () {
-        return {}
+    methods: {
+        UploadForm: function(){
+            let self = this
+            let uploadForm = document.getElementById('uploadForm');
+            let form_data = new FormData(uploadForm);
+
+            fetch("/api/upload", {
+                method: "POST",
+                body: form_data,
+                headers: {
+                    'X-CSRFToken': token
+                    },
+                credentials: 'same-origin'
+            }).then(function(response){
+                return response.json();
+            }).then(function (jsonResponse) {
+                self.messageFlag = true
+
+                if (jsonResponse.hasOwnProperty("errors")){
+                    self.errorFlag=true;
+                    self.message = jsonResponse.errors;
+                }else if(jsonResponse.hasOwnProperty("message")){
+                    self.errorFlag = false;
+                    self.message = "File Upload Successful";
+                    self.cleanForm();
+                }
+             })
+             .catch(function (error) {
+                console.log(error);
+             });
+        },
+        cleanForm : function(){
+            let form =$("#uploadForm")[0];
+            let self = this;
+
+            form.description.value = "";
+            form.photo.value = "";
+            self.filename = "";
+
+        },
+        onFileSelected: function(){
+            let self = this
+            let filenameArr = $("#photo")[0].value.split("\\");
+            self.filename = filenameArr[filenameArr.length-1]
+        }
+    },
+    data: function(){
+        return {
+            errorFlag: false,
+            messageFlag: false,
+            message: [],
+            filename: ""
+        }
     }
-})
+});
 
 // Define Routes
 const router = new VueRouter({
-    mode: 'history',
     routes: [
-        {path: "/", component: Home},
-        // Put other routes here
-
-        // This is a catch all route in case none of the above matches
-        {path: "*", component: NotFound}
+        { path: "/", component: Home },
+        { path: "/upload", component: Upload }
     ]
 });
 
